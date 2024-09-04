a1e3d787a024210b3e8e2e73a279f8030fc90afc
Hamshary1000 <157809997+Hamshary1000@users.noreply.github.com>
2024-09-04 14:26:36 -0700

    Update home.py

--- home.py
+++ home.py
@@ -4,8 +4,8 @@ import streamlit as st
 
 
 st.set_page_config(page_title=None, page_icon=None,
-                   layout="wide")
-                   # initial_sidebar_state="expanded")
+                   layout="wide",
+                   initial_sidebar_state="expanded")
 
 
 
